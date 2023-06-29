e58afde29864abbe600bfe92ddc2d8fd78f8f2e2
s1F10220209 <s1f102202099@iniad.org>
2023-06-29 14:20:16 +0900

    トップページを追加

--- config/urls.py
+++ config/urls.py
@@ -20,3 +20,14 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+
+from django.contrib import admin
+from django.urls import path
+from todo import views as todo_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', todo_views.index, name='index'),
+]
